bd6047e1e73d4f1fc88145166513592b7fc6734f
Chebet Kamara <chebetkamara@gmail.com>
2022-12-01 09:55:48 -0800

    Printing and listing arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    count = len(argv)
+    i = 1
+    if count == 0:
+        print("{:d} arguments.".format(count))
+    elif count == 1:
+        print("{:d} argument:".format(count))
+        print("{:d}: {s}".format(i, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(count))
+        while index <= count:
+            print("{:d} {:s}".format(, sys.argv[i]))
+            i += 1
