0e0e1afce619ac1696f6f5305c69e2b5564889ca
Chebet Kamara <chebetkamara@gmail.com>
2022-12-04 03:36:38 -0800

    Changing strign format

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,5 @@ def print_list_integer(my_list=[]):
     num = 0
     count = len(my_list)
     while num < count:
-        print("{}".format(my_list[num]))
+        print("{:d}".format(my_list[num]))
         num += 1
