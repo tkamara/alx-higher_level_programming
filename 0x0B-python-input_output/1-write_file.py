51a0cb2ba19aaed41bf2c38ed27c26fad357fd7f
Chebet Kamara <chebetkamara@gmail.com>
2023-01-10 03:24:03 -0800

    Appending text to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,4 @@ writing a string to a text file
 
 def write_file(filename="", text=""):
     with open(filename, 'w', encoding='utf-8') as f:
-        data = f.write(text)
-        return (data)
+        return (f.write(text))
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def append_write(filename="", text=""):
+    """function that appends a string at the end of a text file
+    """
+    with open(filename, 'a', encoding='utf-8') as newFile:
+        return (newFile.write(text))
