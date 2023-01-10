8afd5ada026d59ae08bcc8fccf4ca5c053c2d853
Chebet Kamara <chebetkamara@gmail.com>
2023-01-09 23:49:53 -0800

    Python input and output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Reading text file
+"""
+
+
+def read_file(filename=""):
+    """
+    reads text file and prints to stdout
+
+    Argument:
+        filename(str): the file name
+    """
+    with open('my_file_0.txt') as f:
+        for line in f:
+            print(line, end='')
