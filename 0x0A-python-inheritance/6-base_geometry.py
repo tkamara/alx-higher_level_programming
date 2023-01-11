04732402b4242f63abfc07876cd9366fe84b037d
Chebet Kamara <chebetkamara@gmail.com>
2023-01-11 06:20:42 -0800

    Creating a class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+class based on previous class
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        function to calculate area
+        """
+        raise Exception('area() is not implemented')
