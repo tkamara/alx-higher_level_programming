2dbf1dfb6fcd4adf6e1a69cf8546b8340f6c67a6
Chebet Kamara <chebetkamara@gmail.com>
2023-01-11 06:11:13 -0800

    An empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+creating an empty class
+"""
+
+
+class BaseGeometry:
+    """ an empty class
+    """
+    pass
---
+++ 0x0A-python-inheritance/5-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+
+
+print(bg)
+
+print(dir(bg))
+
+print(dir(BaseGeometry))
